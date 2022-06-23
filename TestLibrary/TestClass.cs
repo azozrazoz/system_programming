f73456074869c0f6af2ceb253a154ce30ef15efa
ITstart <itstart@itstep.loc>
2022-06-23 21:34:35 +0600

    add lesson_01 files

---
+++ TestLibrary/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class TestClass
+    {
+        public void Write()
+        {
+            Console.WriteLine("Text");
+        }
+        public static decimal Square(decimal x)
+        {
+            return x * x;
+        }
+    }
+}
