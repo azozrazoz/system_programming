fd1917aa0d58844ca3f3cf0dd084a9ca51bc953f
Дошан Есмаганбет <doshan.de@gmail.com>
2022-06-24 21:24:55 +0600

    add home_work_01

---
+++ Home_work_01/Child_process/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Child_process")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Child_process")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0fb3a0ed-24ad-40fa-b930-cb3e2ac0b0f9")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Home_work_01/Home_work_01/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work_01
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // 1
+
+            /*Console.WriteLine("Hello, this is parent process!");
+
+            Process process = new Process();
+            process.StartInfo.FileName =
+                @"C:\Users\Дошан\source\repos\system_programming\Home_work_01\Child_process\bin\Debug\Child_process.exe";
+
+            process.Start();
+            process.WaitForExit();
+            process.Exited += Process_exited;
+            Console.WriteLine($"Code exit: {process.ExitCode}");
+            Console.ReadLine();*/
+
+
+
+            // 2
+
+            /*Console.WriteLine("Hello, this is parent process!");
+
+            Process process = new Process();
+            process.StartInfo.FileName =
+                @"C:\Users\Дошан\source\repos\system_programming\Home_work_01\Child_process\bin\Debug\Child_process.exe";
+            process.Start();
+
+            string command = Console.ReadLine();
+            if (command == "exit")
+            {
+                process.Kill();
+            }
+
+            process.WaitForExit();
+            process.Exited += Process_exited;
+            Console.WriteLine($"Code exit: {process.ExitCode}");
+            Console.ReadLine();*/
+
+
+
+            // 3
+
+            /*Console.WriteLine("Hello, this is parent process!");
+
+            Console.Write("Enter first argument: ");
+            string arg1 = Console.ReadLine();
+            Console.Write("Enter second argument: ");
+            string arg2 = Console.ReadLine();
+            Console.Write("Enter operation: ");
+            string op = Console.ReadLine();
+
+
+            Process process = new Process();
+            process.StartInfo.FileName =
+                @"C:\Users\Дошан\source\repos\system_programming\Home_work_01\Child_process\bin\Debug\Child_process.exe";
+            process.StartInfo.Arguments = $"{arg1} {arg2} {op}";
+            process.Start();
+            process.Exited += Process_exited;
+
+
+            Console.ReadLine();
+            return 0;*/
+
+
+
+            // 4
+
+            /*Console.WriteLine("Hello, this is parent process!");
+            Console.Write("Enter path to file: ");
+            string pathToFile = Console.ReadLine();
+            Console.Write("Enter path to file: ");
+            string word = Console.ReadLine();
+
+            Process process = new Process();
+            process.StartInfo.FileName =
+                @"C:\Users\Дошан\source\repos\system_programming\Home_work_01\Child_process\bin\Debug\Child_process.exe";
+            process.StartInfo.Arguments = $"{pathToFile} {word}";
+            process.Start();
+            process.Exited += Process_exited;
+
+
+            Console.ReadLine();
+            return 0;*/
+        }
+
+        static void Process_exited(object sender, EventArgs e)
+        {
+            Console.WriteLine("Process exit");
+        }
+    }
+}
