f9a60964662ef93bd6aace651654e7f83efaf587
Beaming_Deamon <51133964+beamingdeamon@users.noreply.github.com>
2022-07-05 21:50:17 +0600

    first commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SecondsInTime = new System.Windows.Forms.Button();
+            this.DefaultTime = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SecondsInTime
+            // 
+            this.SecondsInTime.Location = new System.Drawing.Point(12, 12);
+            this.SecondsInTime.Name = "SecondsInTime";
+            this.SecondsInTime.Size = new System.Drawing.Size(163, 23);
+            this.SecondsInTime.TabIndex = 0;
+            this.SecondsInTime.Text = "Seconds in time";
+            this.SecondsInTime.UseVisualStyleBackColor = true;
+            this.SecondsInTime.Click += new System.EventHandler(this.SecondsInTime_Click);
+            // 
+            // DefaultTime
+            // 
+            this.DefaultTime.Location = new System.Drawing.Point(12, 41);
+            this.DefaultTime.Name = "DefaultTime";
+            this.DefaultTime.Size = new System.Drawing.Size(163, 23);
+            this.DefaultTime.TabIndex = 1;
+            this.DefaultTime.Text = "Default time";
+            this.DefaultTime.UseVisualStyleBackColor = true;
+            this.DefaultTime.Click += new System.EventHandler(this.DefaultTime_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 437);
+            this.Controls.Add(this.DefaultTime);
+            this.Controls.Add(this.SecondsInTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SecondsInTime;
+        private System.Windows.Forms.Button DefaultTime;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SecondsInTime_Click(object sender, EventArgs e)
+        {
+            var currentUser = Registry.CurrentUser;
+            var subKey = currentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced", true);
+            subKey.SetValue("ShowSecondsInSystemClock", "1", RegistryValueKind.DWord);
+            subKey.Close();
+            RestartExplore();
+        }
+
+        private void DefaultTime_Click(object sender, EventArgs e)
+        {
+            var currentUser = Registry.CurrentUser;
+            var subKey = currentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced", true);
+            subKey.SetValue("ShowSecondsInSystemClock", "0", RegistryValueKind.DWord);
+            subKey.Close();
+            RestartExplore();
+        }
+
+        private static void RestartExplore()
+        {
+            /*foreach (Process p in Process.GetProcesses())
+            {
+                if (p.MainModule.ModuleName.Contains("explorer") == true)
+                    p.Kill();
+            }
+            string explorer = string.Format("{0}\\{1}", Environment.GetEnvironmentVariable("WINDIR"), "explorer.exe");
+            Process process = new Process();
+            process.StartInfo.FileName = explorer;
+            process.StartInfo.UseShellExecute = true;
+            process.Start();*/
+
+            Process myProcess = new Process();
+            myProcess.StartInfo.FileName = "cmd.exe";
+            myProcess.StartInfo.Arguments = @"C:\Users\itstart.ITSTEP\Documents\script.bat";
+
+            myProcess.Start();
+        }
+    }
+}
