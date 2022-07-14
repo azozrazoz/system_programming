422e1a6eca2834add859a94a61d3855121260779
Дошан Есмаганбет <doshan.de@gmail.com>
2022-07-14 16:20:29 +0600

    add home_work_06 files

---
+++ Home_work_06/Home_work_06/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace Home_work_06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenBanWords = new System.Windows.Forms.OpenFileDialog();
+            this.Start = new System.Windows.Forms.Button();
+            this.FolderForBanWords = new System.Windows.Forms.FolderBrowserDialog();
+            this.ChooseFolder = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbBanWords = new System.Windows.Forms.TextBox();
+            this.tbMainPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OpenBanWords
+            // 
+            this.OpenBanWords.FileName = "openFileDialog1";
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(236, 12);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // ChooseFolder
+            // 
+            this.ChooseFolder.Location = new System.Drawing.Point(115, 12);
+            this.ChooseFolder.Name = "ChooseFolder";
+            this.ChooseFolder.Size = new System.Drawing.Size(115, 23);
+            this.ChooseFolder.TabIndex = 1;
+            this.ChooseFolder.Text = "Choose Folder";
+            this.ChooseFolder.UseVisualStyleBackColor = true;
+            this.ChooseFolder.Click += new System.EventHandler(this.ChooseFolder_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load ban words";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbBanWords
+            // 
+            this.tbBanWords.Location = new System.Drawing.Point(12, 80);
+            this.tbBanWords.Multiline = true;
+            this.tbBanWords.Name = "tbBanWords";
+            this.tbBanWords.Size = new System.Drawing.Size(299, 299);
+            this.tbBanWords.TabIndex = 3;
+            // 
+            // tbMainPath
+            // 
+            this.tbMainPath.Location = new System.Drawing.Point(12, 54);
+            this.tbMainPath.Name = "tbMainPath";
+            this.tbMainPath.Size = new System.Drawing.Size(299, 20);
+            this.tbMainPath.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Путь к файлам";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbMainPath);
+            this.Controls.Add(this.tbBanWords);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ChooseFolder);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "Ban words";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog OpenBanWords;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.FolderBrowserDialog FolderForBanWords;
+        private System.Windows.Forms.Button ChooseFolder;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbBanWords;
+        private System.Windows.Forms.TextBox tbMainPath;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Home_work_06/Home_work_06/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Home_work_06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            OpenBanWords.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+        string path;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (OpenBanWords.ShowDialog() == DialogResult.OK)
+                tbBanWords.Text = File.ReadAllText(OpenBanWords.FileName);
+        }
+
+        private void ChooseFolder_Click(object sender, EventArgs e)
+        {
+            if (FolderForBanWords.ShowDialog() == DialogResult.OK)
+                path = FolderForBanWords.SelectedPath;
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            string[] banWords = tbBanWords.Text.Split(' ', '\r', '\n');
+
+            if(tbMainPath.Text != "")
+            {
+                if (Directory.Exists(tbMainPath.Text))
+                {
+                    var files = Directory.GetFiles(tbMainPath.Text, "*.txt");
+                    Task[] tasks = new Task[files.Length];                    
+
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        for (int t = 0; t < banWords.Length; t++)
+                        {
+                            tasks[i] = new Task(() => SearchBanWords(files[i], banWords[t]));
+                            tasks[i].Start();
+                            tasks[i].Wait();
+                            Thread.Sleep(1);
+                        }
+                        Task.WaitAll();
+                    }
+
+
+                    if (!File.Exists($"{path}\\result.txt"))
+                    {
+                        File.Create($"{path}\\result.txt");
+                        using (StreamWriter writer = new StreamWriter($"{path}\\result.txt"))
+                        {
+                            writer.Write(result + $"Maximum: {maxResult}");
+                            writer.Close();
+                        }
+                    }
+                    else
+                    {
+                        using (StreamWriter writer = new StreamWriter($"{path}\\result.txt"))
+                        {
+                            writer.Write(result + $"Maximum: {maxResult}");
+                            writer.Close();
+                        }
+                    }
+
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        var pathTo = path + "\\" + files[i].Split('\\').Last();
+                        string finalResult = "";
+                        MyUsers.FileLibrary.CopyFile(files[i], pathTo);
+                        using(StreamReader reader = new StreamReader(pathTo))
+                        {
+                            finalResult = reader.ReadToEnd().ToLower();
+                            for (int j = 0; j < banWords.Length; j++)
+                            {
+                                finalResult = finalResult.Replace(banWords[j], "*******");
+                            }
+                                
+                            reader.Close();
+                        }
+                        using(StreamWriter writer = new StreamWriter(pathTo))
+                        {
+                            writer.WriteLine(finalResult);
+                            writer.Close();
+                        }
+                    }
+
+                    result = "";
+                    maxCount = 0;
+                    maxResult = "";
+                    MessageBox.Show($"Файлы изменены и скопированы в {path}", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+                tbMainPath.Text = "Введите путь к файлам!";
+        }
+
+        string result;
+        string maxResult;
+        int maxCount = 0;
+
+        private void SearchBanWords(string path, string word)
+        {            
+            int count = 0;
+            
+            FileStream file = new FileStream(path, FileMode.OpenOrCreate);
+            StreamReader reader = new StreamReader(file);
+
+            string[] text = reader.ReadToEnd().ToLower().Split(' ', '\r', '\n');
+
+            reader.Close();
+
+            foreach (var t in text)
+                if (t == word)
+                    count++;
+
+            result += $"{word}\t{count}\r\n";
+
+            if(count > maxCount)
+            {
+                maxCount = count;
+                maxResult = $"{word}\t{count}\r\n";
+            }
+            else if(count == maxCount)
+                maxResult += $"{word}\t{count}\r\n";
+        }
+    }
+}
